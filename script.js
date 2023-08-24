ebaf3a7b78640769c307640d26c2db401f49d7a8
Franco <francomaxey@msn.com>
2023-08-24 14:10:33 -0400

    Modified Styling; left off on changing theme

--- script.js
+++ script.js
@@ -2,11 +2,28 @@
     let stopwatch2Value = 0;
     let interval1, interval2;
     let isPaused = true;
+    let theme = 'START';
 
     const stopwatch1Element = document.getElementById("stopwatch1");
     const stopwatch2Element = document.getElementById("stopwatch2");
 
+    const startButton = document.getElementById("startButton");
+    const pauseButton = document.getElementById("pauseButton");
+    const breakButton = document.getElementById("breakButton");
+
+    function changeTheme(){
+        if(theme == 'START'){
+            theme = 'BREAK';
+        }
+        else{
+            theme = 'START';
+        }
+    }
+
     function startStopwatches() {
+        startButton.style.display = "none";
+        pauseButton.style.display = "block";
+        breakButton.style.display = "block";
         if(isPaused) {
             interval1 = setInterval(updateStopwatches, 100);
             isPaused = false;
@@ -33,16 +50,23 @@
     }
 
     function pauseStopwatches() {
+        startButton.style.display = "block";
+        pauseButton.style.display = "none";
+        breakButton.style.display = "block";
         clearInterval(interval1);
         clearInterval(interval2);
         isPaused = true;
     }
 
     function takeBreak() {
+        startButton.style.display = "block";
+        pauseButton.style.display = "block";
+        breakButton.style.display = "none";
         if (isPaused) {
             stopwatch1Value = 0; // Reset stopwatch 1
             stopwatch1Element.textContent = stopwatch1Value.toFixed(2);
             isPaused = false
+
             interval2 = setInterval(onBreak, 100);
         }
     }
